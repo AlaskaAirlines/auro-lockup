4d0c2e72b7645c1fa7e0844b9952030d638f3b11
Dale Sande <dale.sande@gmail.com>
2021-03-11 11:03:55 -0800

    refactor: restrict standard prop to only work with oneworld prop

--- test/auro-lockup.test.js
+++ test/auro-lockup.test.js
@@ -23,10 +23,19 @@ describe('auro-lockup', () => {
 
   it('auro-lockup onworld attribute replaces product and tagline with oneworld logo', async () => {
     const el = await fixture(html`
-      <auro-lockup oneworld>
-        <span slot="title">Product Name</span>
-        <span slot="subtitle">Powered by Partner Name</span>
-      </auro-lockup>
+      <auro-lockup oneworld></auro-lockup>
+    `);
+
+    const logo = el.shadowRoot.querySelector('.oneworldLogo');
+    const headerTitle = el.shadowRoot.querySelector('.headerTitle')
+
+    await expect(logo).to.be.visible;
+    await expect(headerTitle).to.not.exist;
+  });
+
+  it('auro-lockup oneworld logo with standard Alaska logo sans tagline', async () => {
+    const el = await fixture(html`
+      <auro-lockup oneworld standard></auro-lockup>
     `);
 
     const logo = el.shadowRoot.querySelector('.oneworldLogo');
