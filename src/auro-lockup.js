AlaskaAirlines/auro-lockup

commit 02088daed6de5c75385ec7f1ab4d0090ac663c0a

perf(deps): update dependencies

src/auro-lockup.js
@@ -9,9 +9,9 @@
 import { LitElement, html } from "lit";
 import { ifDefined } from 'lit/directives/if-defined.js';
 import { classMap } from 'lit/directives/class-map.js';
-import logoOfficial from '@alaskaairux/icons/dist/restricted/AS-tagline-200.mjs';
-import logoStandard from '@alaskaairux/icons/dist/restricted/AS-200.mjs';
-import logoOneworld from '@alaskaairux/icons/dist/logos/oneworld.mjs';
+import logoOfficial from '@alaskaairux/icons/dist/restricted/AS-tagline-200_es6.js';
+import logoStandard from '@alaskaairux/icons/dist/restricted/AS-200_es6.js';
+import logoOneworld from '@alaskaairux/icons/dist/logos/oneworld_es6.js';
 
 // If using auroElement base class
 // See instructions for importing auroElement base class https://git.io/JULq4

commit b60f1c31696716325277190ffb546e0b076f781e

perf: update icon refs to use ESM supported SVGs
This commit reverts a regression from commit 02088da

Changes to be committed:
modified:   src/auro-lockup.js

src/auro-lockup.js
@@ -9,9 +9,9 @@
 import { LitElement, html } from "lit";
 import { ifDefined } from 'lit/directives/if-defined.js';
 import { classMap } from 'lit/directives/class-map.js';
-import logoOfficial from '@alaskaairux/icons/dist/restricted/AS-tagline-200_es6.js';
-import logoStandard from '@alaskaairux/icons/dist/restricted/AS-200_es6.js';
-import logoOneworld from '@alaskaairux/icons/dist/logos/oneworld_es6.js';
+import logoOfficial from '@alaskaairux/icons/dist/restricted/AS-tagline-200.mjs';
+import logoStandard from '@alaskaairux/icons/dist/restricted/AS-200.mjs';
+import logoOneworld from '@alaskaairux/icons/dist/logos/oneworld.mjs';
 
 // If using auroElement base class
 // See instructions for importing auroElement base class https://git.io/JULq4